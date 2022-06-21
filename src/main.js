ddd98f7a1d5607eabd16058f4013671ad9aaa9d8
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2022-06-21 15:50:48 +0300

    8.1.1 PointModel to main

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import FilterPresenter from './presenter/filter-presenter';
 import NewPointButtonView from './view/new-point-button/new-point-button-view';
 import { render, RenderPosition } from './framework/render';
 import MenuView from './view/menu/menu-view';
+import PointsApiService from './points-api-service';
+
+
+const AUTHORIZATION = 'Basic 64sg1665gsggfsg';
+const END_POINT = 'https://17.ecmascript.pages.academy/big-trip';
 
 const pageHeader = document.querySelector('.page-header');
 const siteTripMainElement = pageHeader.querySelector('.trip-main');
@@ -14,7 +19,7 @@ const tripControls = pageHeader.querySelector('.trip-controls__filters'); //Бл
 const pageMain = document.querySelector('.page-main');
 const tripEvents = pageMain.querySelector('.trip-events'); //Блок в мейне страницы для отрисовки информации про точки/места
 
-const pointsModel = new PointModel(); //Массив точек/мест
+const pointsModel = new PointModel(new PointsApiService(END_POINT, AUTHORIZATION));
 const filterModel = new FilterModel();
 const destinationModel = new DestinationModel(); //Массив информации про точки/места
 
--- src/model/point-items-model.js
+++ src/model/point-items-model.js
@@ -2,7 +2,21 @@ import { generatePoint } from '../mock/point-mock';
 import Observable from '../framework/observable.js';
 
 export default class PointModel extends Observable {
+  #pointsApiService = null;
   #points = Array.from({ length: 4 }, generatePoint);
+
+  constructor(pointsApiService) {
+    super();
+    this.#pointsApiService = pointsApiService;
+
+    this.#pointsApiService.points.then((points) => {
+      console.log(points);
+      // Есть проблема: cтруктура объекта похожа, но некоторые ключи называются иначе,
+      // а ещё на сервере используется snake_case, а у нас camelCase.
+      // Можно, конечно, переписать часть нашего клиентского приложения, но зачем?
+      // Есть вариант получше - паттерн "Адаптер"
+    });
+  }
 
   get points() {
     return this.#points;
