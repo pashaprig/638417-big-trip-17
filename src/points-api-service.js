16f1765658a505a0619a3d7c9bae784b9b660af5
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2022-06-21 16:22:28 +0300

    8.1.3 Adapter

--- src/points-api-service.js
+++ src/points-api-service.js
@@ -15,12 +15,30 @@ export default class PointsApiService extends ApiService {
     const response = await this._load({
       url: `points/${point.id}`,
       method: Method.PUT,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({'Content-Type': 'application/json'}),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  };
+
+  #adaptToServer = (point) => {
+    const adaptedPoint = {...point,
+      'base_price': point.basePrice,
+      'date_from': point.dateFrom instanceof Date ? point.dateFrom.toISOString() : null, // На сервере дата хранится в ISO формате
+      'date_to': point.dateTo instanceof Date ? point.dateTo.toISOString() : null, // На сервере дата хранится в ISO формате
+      'is_favorite': point.isFavorite,
+
+    };
+
+    // Ненужные ключи мы удаляем
+    delete adaptedPoint.basePrice;
+    delete adaptedPoint.dateFrom;
+    delete adaptedPoint.dateTo;
+    delete adaptedPoint.isFavorite;
+
+    return adaptedPoint;
   };
 }
